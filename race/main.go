45b7a58da75dcb52acfb9c1516cee93320ff8c40
Marcos Ribeiro <slproger@gmail.com>
2025-01-05 22:55:21 -0300

    OS and ARCH, build for linux/windows

--- race/main.go
+++ race/main.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	fmt.Println("Arch: ", runtime.GOARCH)
+	fmt.Println("OS: ", runtime.GOOS)
 	fmt.Println("CPUs: ", runtime.NumCPU())
 	fmt.Println("Goroutines: ", runtime.GOMAXPROCS(0))
 
